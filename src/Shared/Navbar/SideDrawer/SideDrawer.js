e6674817ece8599c0ad1a7c23b123fac72947877
unknown <jobroman83@gmail.com>
2019-10-14 14:42:58 -0500

    fixing SideDrawer

--- src/Shared/Navbar/SideDrawer/SideDrawer.js
+++ src/Shared/Navbar/SideDrawer/SideDrawer.js
@@ -10,15 +10,17 @@ import './SideDrawer.scss'
 
   return (
         <nav className= {drawerClasses.join(' ')}>
+            <div className="menuToggle">
                <ul>
                  <li><NavLink exact to ="/Home" className="item" activeClassName="is-selected">Home</NavLink></li>
-                 <li><NavLink exact to ="/Variaciones" className="item" activeClassName="is-selected">Variaciones Coreografícas</NavLink></li>
+                 <li><NavLink exact to ="/Variaciones" className="item" activeClassName="is-selected">Variaciones C.</NavLink></li>
                  <li><NavLink exact to ="/Photos" className="item" activeClassName="is-selected">Fotografías</NavLink></li>
                  <li><NavLink exact to ="/videos" className="item" activeClassName="is-selected">Videos</NavLink></li>
                  <li><NavLink exact to ="/About-me" className="item" activeClassName="is-selected">Biografía</NavLink>  </li>
                  <li><NavLink exact to ="/Contact-me" className="item" activeClsassName="is-selected"> Contacto</NavLink></li>
                  
               </ul>            
+              </div>
          </nav>
          );
        };
