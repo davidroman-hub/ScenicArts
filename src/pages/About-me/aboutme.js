2240a3d6cc8677d5f8eea16f2a25b532b4fb934e
unknown <jobroman83@gmail.com>
2019-10-14 15:54:02 -0500

    finishing about-me

--- src/pages/About-me/aboutme.js
+++ src/pages/About-me/aboutme.js
@@ -4,8 +4,14 @@ import './AboutMe.scss'
 import Ensamble from './Photos/Ensamble1.jpg'
 import FlorCalavera from'./Photos/FlorCalavera2.jpg'
 import Contextos from './Photos/Contextos.jpg'
-import mezCal from './Photos/mezcalR.jpg'
+import variaciones13 from './Photos/variaciones13.jpg'
 import bellasArtes from './Photos/bellasArtes.jpg'
+import Raices from './Photos/raices.jpg'
+import Ecuador from './Photos/Ecuador.jpg'
+import Descontentos from './Photos/DescontentoS.jpg'
+import Revolcadero from './Photos/RevolcaderoStat.jpg'
+import instalacion from './Photos/InstalacionP.jpg'
+import posee from './Photos/Variaciones12.jpg'
 //component 
 
 class AboutMe extends Component{
@@ -72,8 +78,149 @@ class AboutMe extends Component{
         como fotógrafa y asistente de producción en “Ftc Studio”, asistiendo
         a muchos eventos sociales, como conciertos de música, danza, teatro,
         graduaciones, entre otros más.
-        <img alt="bellasArtes" src={bellasArtes}/>
+    
         </p>
+        <p>
+           <img alt="bellasArtes" src={bellasArtes}/>
+        </p>
+        <p>Se ha presentado en diferentes foros y espacios: Foro Experimental 
+          Black Box del CENART, Teatro de la Danza, Teatro El Granero y Teatro 
+          Orientación del CCB, Salón de la Danza UNAM, Instituto Tecnológico de
+          Artes del Ecuador de la Ciudad de Guayaquil, Museo de Historia de Tlalpan, 
+          Sala Cultural Ollin Yoliztli, Teatro Julio Jiménez Rueda, Auditorio de la 
+          Fes Acatlán UNAM, Auditorio Che Guevara de C.U., Siniestro & Co, We Are Todos,
+          Universidad del Valle de México “Festival Danzas Linces”, Efel Danse, Foro 
+          Alternativo Espacio Abierto, en el Barrio de la Concepción Tlacoapa junto
+          al Colectivo “Xochimilco No Te Mueras” (en el movimiento del rescate de 
+          espacios públicos), Rotonda de los Personajes Ilustres de Xochimilco,
+          Teatro Carlos Pellicer, Museo José Luis Cuevas, el Antiguo Colegio de 
+          las Vizcaínas, Teatro Julio Castillo , Museo Panteón de San Fernando,
+          Foro Cultural Coyoacanense, Foro Cultural Quetzalcóatl y en El Instituto
+          Nacional de Cancerología México y La Sociedad Médica del INCan y en Monte 
+          Sur Xochimilco S.C.
+          </p>
+          <p>
+             <img alt="variaciones" src={variaciones13}/>
+          </p>
+          <p>
+          Sus más recientes piezas coreográficas son “Tehuantin” pieza en la que colabora
+          con Alberto Carrillo y Los 400 Conejos Ebrios, Septiembre 2019. “Atemporal” (2019)
+          dueto con Dalia Ramírez y en colaboración con los músicos Carian Paz, Gabriel Tepichin 
+          y David Morales. “S | A” (2018) fueron improvisaciones coreográficas en las que dirigió a
+          Francisco García, Michelle Aché y Dalia Ramírez.
+          “J | R Co Identidades” en el cuál trabajo con Juan Carlos García Director de House of 
+          Apocalipstick y Rodrigo Casillas de la banda El Monstruo Son Los Otros, febrero de 2017 a Julio de 2018.
+          Colaboro en el proyecto “Ánima” como intérprete dentro del marco de creación de las
+          prácticas escénicas del CICO-INBA. Dirigió la obra coreográfica “De las Olas” dentro 
+          el Centro de Investigación Coreográfica, Julio, 2016, México D.F. Participo como intérprete
+           en la “Estación Revolcadero” de las prácticas Escénicas del CICO– INBA, Noviembre 2015.
+          </p>
+          <p>
+            <img alt="" src={Raices}/>
+          </p>
+          <p>
+          Fue Intérprete en la coreografía “Con Textos” de Sarai González, de las Prácticas Creativas 
+          (Función dedicada a la Profesora Jane Haw Bedford). Jueves 5 de Febrero 2015, Teatro de la Danza
+          y Foro Experimental Black Box del CENART.
+          Creó la 2DA. FERIA DE ARTES ESCENICAS en el CONJUNTO CULTURAL CARLOS PELLICER el domingo 27 de
+          septiembre de 2015 y bailo en la presentación del Dueto FLOR CALAVERA “Mezclar Raíces”.
+          Creó también la 1ER. FERIA DE ARTES ESCENICAS igualmente en el CONJUNTO CULTURAL CARLOS PELLICER 
+          el viernes
+          12 de diciembre de 2014, y bailó en la presentación del solo “Mezclar Raíces”.
+          Organizo el 1ER. FESTIVAL DEL DÍA INTERNACIONAL DE LA DANZA 2014 en la “ROTONDA DE LOS PERSONAJES
+          ILUSTRES DE XOCHIMILCO”, presentando la obra “Aquelarre de una Noche” y siendo la directora artística
+           del festival. Xochimilco D.F, 26 de Abril del 2014.
+          Participo en la FERIA INTERDELEGACIONAL DE MIPYME´S CULTURALES, ARTÍSTICAS Y TECNOLÓGICAS presentándose 
+          a FLOR CALAVERA como ponente en el marco de la Feria, México D.F, 14 de Diciembre 2013. FLOR CALAVERA 
+          presentando “Aquelarre de Una Noche” en la conservación de las tradiciones de Xochimilco, Septiembre y
+          Octubre 2013, con el Colectivo Xochimilco No Te Mueras. FLOR CALAVERA (DUETO “LOS CALAVERA”) presentándose
+          en el (XI Encuentro Internacional de Danza “FRAGMENTOS DE JUNIO 2013”, Jueves 27 de Junio, en la Sala
+           de Danza del Instituto tecnológico de Artes del Ecuador (ITAE) en la ciudad de Guayaquil, Ecuador.
+          </p>
+          <p>
+            <img alt="AquelarreEcuador" src={Ecuador}/>
+          </p>
+          <p>
+          FLOR CALAVERA presentando “Aquelarre de una Noche” (Festival Danza la Vida) Celebrando el día Internacional 
+          de la Danza, Sábado 27 de abril, 2013 en el Foro Alternativo Espacio Abierto. 
+          Abigail Calavera presentando “La Danza del Noise” en el 6° Encuentro Plural de Danza de La Ciudad de 
+          México, viernes 26 de abril, 2013 en el Centro Cultural Ollin Yoliztli Salón Principal.
+
+          Impartió un Taller Coreográfico de Danza Contemporánea en el Conjunto Cultural Carlos Pellicer, 
+          2015 – 2016. En el Instituto Cultural Sucre El Taller “A moverse ahora que hay modo, 
+          “Congreso de Jóvenes Somos la Generación del Milenio…y?”. Ciudad Satélite, Septiembre 28, 2013.
+          Desde el 2012 al actual colabora con Carian Paz músico y compositor de la banda mexicana “Los 
+          400 Conejos Ebrios, iniciando en el 2012 con “FLOR CALAVERA” que surgió como una inquietud de 
+          transformar la monotonía del diario y la diversidad que en ella existe y de este modo intentar 
+          transformar la perspectiva del espectador.
+          
+          Del año 2011 al 2013 fue Intérprete del Colectivo Ensamble de Teatro Físico bajo la dirección 
+          de Manuel Márquez. Colectivo Ensamble De Teatro Físico presentándose en el Salón de la Danza Unam, 
+          con la obra “La Ciencia del Cielo Y La Tierra”, Diciembre 2012. Colectivo Ensamble De Teatro Físico 
+          presentando “La Ciencia del Cielo Y La Tierra” en el foro Efel Danse, Noviembre 2012.
+
+          </p>
+          <p>
+            <img alt="" src={Descontentos}/>
+          </p>
+          <p>
+          Colectivo Ensamble De Teatro Físico presentando “La Ciencia del Cielo Y La Tierra” en el Teatro Julio 
+          Jiménez Rueda Participación en muestra de Danza a través de la EIA no.2, México D.F a 1ro de Diciembre 
+          del 2011. Colectivo Ensamble De Teatro Físico presentando “La Ciencia del Cielo Y La Tierra” en el Foro 
+          Cultural Coyoacanense. Participación en el Día Internacional de la Danza. México D.F a 27 de Abril del 2012. 
+          Colectivo Ensamble de Teatro Físico presentando “La Ciencia del Cielo Y La Tierra” en el 5° Encuentro Plural 
+          de Danza de la Ciudad de México, Museo Panteón de San Fernando. En la Plaza de San Fernando núm. 17, col. 
+          Guerrero. 27 de abril de 14:00 a 17:00 hrs.
+          Colectivo Ensamble De Teatro Físico a través de la Escuela de Iniciación Artística No.2, Dr. Carmona y Valle 
+          no.54, Participación en Danza Contemporánea dentro del “Festival de Las Artes”. México D.F a 24 de Mayo del 2012. 
+          El Consejo Nacional Para La Cultura Y Las Artes y El INSTITUTO NACIONAL DE BELLAS ARTES Y LITERATURA a través de 
+          las Escuela de Iniciación de Artística No.2 Otorgan un reconocimiento a Saira Abigail Hernández Pérez por su 
+          participación en el “Festival de las Artes, 2012”. Mayo 24, 2012.
+          </p>
+          <p>
+            <img alt="" src={Revolcadero}/>
+          </p>
+          <p>
+          “Mi cuerpo es inmenso y grande como el universo “YO”, Coreografía de Luz Ureña”, EIA No.2 presentándose en el  
+          Teatro Julio Castillo, 18 y 19 de junio del 2013. El Consejo Nacional Para La Cultura Y Las Artes y El INSTITUTO 
+          NACIONAL DE BELLAS ARTES Y LITERATURA a través de las Escuela de Iniciación de Artística No.2 Otorgan un reconocimiento 
+          a Saira Abigail Hernández Pérez por haber concluido íntegramente sus estudios en Iniciación a Danza Contemporánea Categoría 
+          Juvenil, Generación 2010-2013. Escuela de Iniciación artística No.2 en la Celebración de día de muertos in memoriam a Carlos 
+          Monsivais, “A donde vais Monsivais”  Bellas Estampas Mexicanas, Coreografía Manuel Márquez. 
+
+          En el 2009 creó el Animal Colectivo presentando “Leaf House”, 2009. “Tema Para Un Nocturno” inspirado en el poema de Carlos 
+          Pellicer, 2011. “Viaje imaginario” 2008. Animal Colectivo en la Participación en la “Conservación de las Tradiciones de Xochimilco”, 
+          México D.F, Noviembre del 2011. Animal Colectivo en el Auditorio “Che Guevara”, Facultad de Filosofía y Letras ,C.U, UNAM.
+
+          </p>
+          <p>
+            <img alt="" src={instalacion}/>
+          </p>
+          <p>
+          Participación en muestra de Danza Itinerante, México D.F, Noviembre del 2011. Animal Colectivo Universidad Del Valle De México, 
+          Campus Coyoacán participando en el “Primer Encuentro de Danzas Linces Coyoacán 2010, 17 de Abril del 2010. Animal Colectivo, 
+          Universidad Del Valle De México, Campus Coyoacán, Participante en “InterLinces 2010”, México D.F. a 10 de Octubre del 2010. 
+          Animal Colectivo Universidad Del Valle De México, Campus Coyoacán, Participación en el Segundo Encuentro de “Danzas Linces 
+          Coyoacán” México D.F. a 18 de Marzo del 2011.
+
+          Del 2008 al 2009 Cofundadora e intérprete en la compañía de Danza Árabe “Riih Nayymah & Mata´i Te Fetia. En 2005 formo parte de 
+          la compañía de Danza Árabe Rakshanda bajo la dirección de Verónica Morales en el Centro Cultural Carlos Pellicer. Compañía de 
+          Danza Árabe “Riih Nayymah” Foro Cultural Quetzalcoatl participación en Bellynesian, Sábado 13 de Diciembre del 2008.
+          4ta. Semana de la Danza, presentando Danzas Orientales “Riih Nayymah” por Abigail Hernández y Carla Esparza, 24 de Abril del 2008. 
+          </p>
+          <p>
+            <img alt="" src={posee}/>
+          </p>
+          <p>
+          
+          Taller de Danza Árabe “Rakshanda” Centro Cultural Carlos Pellicer,  Xochimilco D.F. a 18 de Diciembre del 2005. Grupo de Danza Árabe 
+          “Rakshanda”, Monte Sur Xochimilco S.C, Fundadora de la Academia Dance Studio, Marzo del 2006. Grupo de Danza Árabe “Rakshanda” 2da. 
+          Feria de la Mujer Contrerense, Participación en el día Internacional de la Mujer “La Mujer mexicana es mujer de lucha”, México D.F. 
+          a 8 de Marzo del.2006
+          Grupo de Danza Árabe “Rakshanda” en El Instituto Nacional de Cancerología México y La Sociedad Médica del INCan, Miércoles 10 de 
+          Mayo del 2006. Grupo de Danza Árabe “Rakshanda” Centro Cultural Carlos Pellicer, México D.F. a 16 de Diciembre del 2006.
+
+          </p>
         </div>
         
       </Fragment>
