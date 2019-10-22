f29dcfbe1bd8193f9021644e0baafd720d2d867f
unknown <jobroman83@gmail.com>
2019-10-21 22:49:18 -0500

    'fixingphotossize'

--- src/pages/Photos/AddPhotos.js
+++ src/pages/Photos/AddPhotos.js
@@ -1,76 +1,76 @@
 export const photos = [
     {
-      src: "https://i.imgur.com/MnvOBOM.jpg",
+      src: "https://i.imgur.com/MnvOBOM.jpg",//1
       width: 4,
       height: 3
     },
     {
-      src: "https://i.imgur.com/PZCoqQc.jpg",
-      width: 1,
-      height: 1
-    },
-    {
-      src: "https://i.imgur.com/4D37xqb.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://i.imgur.com/wUfmYSz.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://i.imgur.com/F6d1s73.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://i.imgur.com/n2hMQxF.jpg",
+      src: "https://i.imgur.com/PZCoqQc.jpg",//2
       width: 4,
       height: 3
     },
     {
-      src: "https://i.imgur.com/5BebQ8f.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://i.imgur.com/Xhp4Gsp.jpg",
+      src: "https://i.imgur.com/4D37xqb.jpg",//3
       width: 4,
       height: 3
     },
     {
-      src: "https://i.imgur.com/FOT0mE4.jpg",
+      src: "https://i.imgur.com/wUfmYSz.jpg",//4
       width: 4,
       height: 3
     },
     {
-      src: "https://i.imgur.com/wUfmYSz.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://i.imgur.com/ZOIqXal.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://i.imgur.com/BBszU7p.jpg",
+      src: "https://i.imgur.com/F6d1s73.jpg",//5
       width: 4,
       height: 3
     },
     {
-      src: "https://i.imgur.com/zPt7tTR.jpg",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://i.imgur.com/r1I9T0d.jpg",
+      src: "https://i.imgur.com/n2hMQxF.jpg",//6
       width: 4,
       height: 3
     },
     {
-      src: "https://i.imgur.com/8B4CUMd.jpg",
+      src: "https://i.imgur.com/5BebQ8f.jpg",//7
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://i.imgur.com/Xhp4Gsp.jpg",//8
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://i.imgur.com/FOT0mE4.jpg",//9
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://i.imgur.com/wUfmYSz.jpg",//10
+      width: 4,
+      height: 2
+    },
+    {
+      src: "https://i.imgur.com/ZOIqXal.jpg",//11
+      width: 4,
+      height:2
+    },
+    {
+      src: "https://i.imgur.com/BBszU7p.jpg",//12
+      width: 4,
+      height: 5
+    },
+    {
+      src: "https://i.imgur.com/zPt7tTR.jpg",//13
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://i.imgur.com/r1I9T0d.jpg",//14
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://i.imgur.com/8B4CUMd.jpg",//15
       width: 4,
       height: 3
     }
