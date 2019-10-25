843aeb372c8cf6da5203d5fefc4ec03328b6e61c
unknown <jobroman83@gmail.com>
2019-10-25 17:42:53 -0500

    'testiong10'

---
+++ src/pages/Contact-me/netlifyForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import NetlifyForm from 'react-netlify-form'
+ 
+export default () => (
+<NetlifyForm name='Contact Form'>
+  {({ loading, error, success }) => (
+    <div>
+      {loading &&
+        <div>Loading...</div>
+      }
+      {error &&
+        <div>Your information was not sent. Please try again later.</div>
+      }
+      {success &&
+        <div>Thank you for contacting us!</div>
+      }
+      {!loading && !success &&
+        <div>
+          <input type='text' name='Name' required />
+          <textarea name='Message' required />
+          <button>Submit</button>
+        </div>
+      }
+    </div>
+  )}
+</NetlifyForm>
+
+);
