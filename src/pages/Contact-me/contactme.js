fab96056113340ee6c82c12c1e24e9cf487b413d
unknown <jobroman83@gmail.com>
2019-10-25 16:57:23 -0500

    'testing3'

--- src/pages/Contact-me/contactme.js
+++ src/pages/Contact-me/contactme.js
@@ -107,18 +107,12 @@
 class ContactMe extends Component{
   render() {
       return(
-        <form name="contact" method="POST" data-netlify="true">
+        <form name="contact" method="post">
         <p>
-          <label>Your Name: <input type="text" name="name" /></label>   
+          <label>Your Name: <input type="text" name="name"/></label>
         </p>
         <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label>
+          <label>Your Email: <input type="email" name="email"/></label>
         </p>
         <p>
           <label>Message: <textarea name="message"></textarea></label>
